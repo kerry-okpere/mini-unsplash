0ed7b96243396cdc407fa6d1c9a8e9fd7a1e9baf
kerry-okpere <kerryokpere@gmail.com>
2020-12-22 21:05:31 +0100

    feat: create landing page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="@/assets/logo.png"> -->
+    <LandingPage />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from '@/Pages/LandingPage/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
@@ -19,8 +21,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/Pages/LandingPage/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="landing">
+    <header class="landing__backdrop">
+    </header>
+    <div class="landing__backdrop-content">
+      <Search />
+      <section class="landing__backdrop-content-photo">
+        <Placeholder v-for="i in 3" :key="i"/>
+        <!-- <Photo /> -->
+      </section>
+      
+    </div>
+  </main>
+</template>
+
+<script>
+// use composition api
+import Search from '@/components/Search';
+import Placeholder from '@/components/Placeholder';
+import Photo from '@/components/Photo';
+
+export default {
+  name: 'LandingPage',
+  components: {
+    Search,
+    Placeholder,
+    Photo
+  },
+  setup(props, context){
+    console.log('i was set upppppp')
+
+    return{}
+  },
+  created(){
+    console.log('i was set upppppp')
+  },
+  methods:{
+    fetchPhotos(){
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import '../../assets/global.css';
+@import './index.css';
+</style>
