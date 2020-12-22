3ad100d848540246ff270b79da36d0a17a572b06
kerry-okpere <kerryokpere@gmail.com>
2020-12-22 21:04:50 +0100

    feat: create search component

---
+++ src/components/Search/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+    <img alt="Vue logo" src="@/assets/img/search.svg" class="search__icon">
+    <input type="search" placeholder="Search for photo" class="search__input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style scoped>
+@import '../../assets/global.css';
+@import './index.css';
+</style>
